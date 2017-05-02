effe9a3abc9ac61a32a603f9f0bd6ba7d7b6d027
Mx <mx@Tests-MacBook-Pro.local>
2017-05-02 22:23:41 +0100

    Panorama Application and AR Android App

---
+++ Augmented_Reality_Android_App/unity_source_code/Assets/ButtonListener.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vuforia;
+
+public class ButtonListener : MonoBehaviour, ITrackableEventHandler {
+
+	public string gameObjectName;
+	private TrackableBehaviour trackableBehaviour;
+	private Animator animator;
+
+	private bool showButton = false;
+	private Rect startDancingButtonRect = new Rect(50,50,120,60);
+	private Rect stopDancingButtonRect = new Rect(50,150,120,60);
+
+	void Start () {
+		trackableBehaviour = GetComponent<TrackableBehaviour>();
+		if (trackableBehaviour)
+		{
+			trackableBehaviour.RegisterTrackableEventHandler(this);
+		}
+		animator = GameObject.Find (gameObjectName).GetComponent<Animator> ();
+	}
+
+	public void OnTrackableStateChanged(
+		TrackableBehaviour.Status previousStatus,
+		TrackableBehaviour.Status newStatus)
+	{
+		if (newStatus == TrackableBehaviour.Status.DETECTED ||
+			newStatus == TrackableBehaviour.Status.TRACKED)
+		{
+			showButton = true;
+		}
+		else
+		{
+			showButton = false;
+		}
+	}
+
+	void OnGUI() {
+		if (showButton) {
+			// start dancing button
+			if (GUI.Button(startDancingButtonRect, "Start Dancing")) {
+				animator.SetTrigger ("startAnimating");
+			}
+
+			// stop dancing button
+			if (GUI.Button(stopDancingButtonRect, "Stop Dancing")) {
+				animator.SetTrigger ("stopAnimating");
+			}
+		}
+	}
+}
